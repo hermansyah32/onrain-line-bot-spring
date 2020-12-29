90ab9e7c5d0d9e45f41d2065a175c349fcd5abfa
Hermansyah <herman.piliang23@gmail.com>
2020-12-30 01:07:18 +0700

    Initial commit

---
+++ src/main/java/dev/hermansyah/onrain/OnRainApplication.java
@@ -0,0 +1,13 @@
+package dev.hermansyah.onrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnRainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnRainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hermansyah/onrain/OnRainApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hermansyah.onrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnRainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
